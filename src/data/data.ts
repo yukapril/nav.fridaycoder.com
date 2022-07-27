1ac0f01183a6283449bed324f749cdfb0930908f
yukapril <i@yukapril.com>
2022-07-27 23:39:32 +0800

    feat: 增加数据条目

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,10 +1,34 @@
 import { Data } from '@/types/data'
 
 const data: Data = [
-  { id: 1, name: 'Excalidraw', desc: '绘制手绘风格的流程图', url: 'https://excalidraw.com', tags: ['流程图'] },
-  { id: 2, name: 'PDFREAL', desc: '实现 PDF 合并、分割、压缩、转换、水印等多种功能', url: 'https://pdfreal.com', tags: ['文档处理', 'PDF'] },
-  { id: 3, name: 'Smallpdf', desc: '实现 PDF 转换、合并、压缩、编辑等多种功能', url: 'https://smallpdf.com', tags: ['文档处理', 'PDF'] },
-  { id: 4, name: 'Regex-Vis', desc: '正则表达式可视化，实时查看输入的正则解释', url: 'https://regex-vis.com', tags: ['开发', '正则'] },
+  {
+    id: 1,
+    name: 'Excalidraw',
+    desc: '绘制手绘风格的流程图',
+    url: 'https://excalidraw.com',
+    tags: ['流程图']
+  },
+  {
+    id: 2,
+    name: 'PDFREAL',
+    desc: '实现 PDF 合并、分割、压缩、转换、水印等多种功能',
+    url: 'https://pdfreal.com',
+    tags: ['文档处理', 'PDF']
+  },
+  {
+    id: 3,
+    name: 'Smallpdf',
+    desc: '实现 PDF 转换、合并、压缩、编辑等多种功能',
+    url: 'https://smallpdf.com',
+    tags: ['文档处理', 'PDF']
+  },
+  {
+    id: 4,
+    name: 'Regex-Vis',
+    desc: '正则表达式可视化，实时查看输入的正则解释',
+    url: 'https://regex-vis.com',
+    tags: ['开发', '正则']
+  },
   {
     id: 5,
     name: 'Cool Backgrounds',
@@ -12,34 +36,160 @@ const data: Data = [
     url: 'https://coolbackgrounds.io',
     tags: ['图片', '设计', '背景', '插画']
   },
-  { id: 6, name: 'Magdeleine', desc: '免费的照片分享站', url: 'https://magdeleine.co', tags: ['图片', '背景'] },
-  { id: 7, name: 'Unsplash', desc: '免费的照片分享站', url: 'https://unsplash.com', tags: ['图片', '背景'] },
-  { id: 8, name: 'Dribbble', desc: '高质量的创作设计社交分享站', url: 'https://dribbble.com', tags: ['图片', '设计', '背景', '插画'] },
-  { id: 9, name: 'freepik', desc: '免费的插画分享站', url: 'https://undraw.co/illustrations', tags: ['图片', '插画'] },
-  { id: 10, name: 'Humaaans', desc: '免费的插画分享站', url: 'https://www.humaaans.com', tags: ['图片', '插画'] },
-  { id: 11, name: 'Open Doodles', desc: '免费的插画分享站', url: 'https://www.opendoodles.com', tags: ['图片', '插画'] },
-  { id: 12, name: 'Pexels', desc: '免费的照片分享站', url: 'https://www.pexels.com', tags: ['图片', '背景'] },
-  { id: 13, name: 'Picography', desc: '免费的照片分享站', url: 'https://picography.co', tags: ['图片', '背景'] },
-  { id: 14, name: 'DOTOWN', desc: '免费的像素画分享', url: 'https://dotown.maeda-design-room.net', tags: ['图片', '像素画', '插画'] },
-  { id: 15, name: 'blush', desc: '免费的插画分享站，可在线创建自定义形象', url: 'https://blush.design', tags: ['图片', '插画'] },
+  {
+    id: 6,
+    name: 'Magdeleine',
+    desc: '免费的照片分享站',
+    url: 'https://magdeleine.co',
+    tags: ['图片', '背景']
+  },
+  {
+    id: 7,
+    name: 'Unsplash',
+    desc: '免费的照片分享站',
+    url: 'https://unsplash.com',
+    tags: ['图片', '背景']
+  },
+  {
+    id: 8,
+    name: 'Dribbble',
+    desc: '高质量的创作设计社交分享站',
+    url: 'https://dribbble.com',
+    tags: ['图片', '设计', '背景', '插画']
+  },
+  {
+    id: 9,
+    name: 'freepik',
+    desc: '免费的插画分享站',
+    url: 'https://undraw.co/illustrations',
+    tags: ['图片', '插画']
+  },
+  {
+    id: 10,
+    name: 'Humaaans',
+    desc: '免费的插画分享站',
+    url: 'https://www.humaaans.com',
+    tags: ['图片', '插画']
+  },
+  {
+    id: 11,
+    name: 'Open Doodles',
+    desc: '免费的插画分享站',
+    url: 'https://www.opendoodles.com',
+    tags: ['图片', '插画']
+  },
+  {
+    id: 12,
+    name: 'Pexels',
+    desc: '免费的照片分享站',
+    url: 'https://www.pexels.com',
+    tags: ['图片', '背景']
+  },
+  {
+    id: 13,
+    name: 'Picography',
+    desc: '免费的照片分享站',
+    url: 'https://picography.co',
+    tags: ['图片', '背景']
+  },
+  {
+    id: 14,
+    name: 'DOTOWN',
+    desc: '免费的像素画分享',
+    url: 'https://dotown.maeda-design-room.net',
+    tags: ['图片', '像素画', '插画']
+  },
+  {
+    id: 15,
+    name: 'blush',
+    desc: '免费的插画分享站，可在线创建自定义形象',
+    url: 'https://blush.design',
+    tags: ['图片', '插画']
+  },
   {
     id: 16,
     name: 'NGINXConfig',
     desc: '在线生成 NGINX 配置',
     url: 'https://www.digitalocean.com/community/tools/nginx',
-    tags: ['开发', 'NGINX']
+    tags: ['运维', 'NGINX']
   },
-  { id: 17, name: 'DiG GUI', desc: '在线版的 dig 命令', url: 'https://www.diggui.com', tags: ['开发', 'dig'] },
-  { id: 18, name: 'Anon Addy', desc: '邮件转发，可惜免费版不能自定义邮件名', url: 'https://anonaddy.com', tags: ['邮件'] },
-  { id: 19, name: 'draw.io', desc: '绘制标准的流程图，功能非常丰富', url: 'https://app.diagrams.net', tags: ['流程图'] },
-  { id: 20, name: 'isoflow.io', desc: '绘制流程示意图，主要是网络拓扑展示', url: 'https://isoflow.io', tags: ['流程图'] },
-  { id: 21, name: 'Carbon', desc: '把代码转成好看的图片', url: 'https://carbon.now.sh', tags: ['开发', '代码转图'] },
-  { id: 22, name: 'Codeimg', desc: '把代码转成好看的图片', url: 'https://codeimg.io', tags: ['开发', '代码转图'] },
-  { id: 23, name: 'asciiflow', desc: '使用 ASCII 绘制流程图，可以直接粘贴文本出来', url: 'https://asciiflow.com', tags: ['流程图'] },
-  { id: 24, name: 'Debuggex', desc: '正则表达式在线调试', url: 'https://www.debuggex.com', tags: ['开发', '正则'] },
-  { id: 25, name: 'Happy Hues', desc: '提供大量配色方案可以选择', url: 'https://www.happyhues.co', tags: ['配色'] },
-  { id: 26, name: 'Picular', desc: '根据输入的名字提供配色方案，但是好像不是太准', url: 'https://picular.co', tags: ['配色'] },
-  { id: 27, name: 'Shields.io', desc: '在线生成项目的标签，比如代码覆盖率、大小、协议、版本等', url: 'https://shields.io', tags: ['开发', '标签'] },
+  {
+    id: 17,
+    name: 'DiG GUI',
+    desc: '在线版的 dig 命令',
+    url: 'https://www.diggui.com',
+    tags: ['开发', 'dig']
+  },
+  {
+    id: 18,
+    name: 'Anon Addy',
+    desc: '邮件转发，可惜免费版不能自定义邮件名',
+    url: 'https://anonaddy.com',
+    tags: ['邮件']
+  },
+  {
+    id: 19,
+    name: 'draw.io',
+    desc: '绘制标准的流程图，功能非常丰富',
+    url: 'https://app.diagrams.net',
+    tags: ['流程图']
+  },
+  {
+    id: 20,
+    name: 'isoflow.io',
+    desc: '绘制流程示意图，主要是网络拓扑展示',
+    url: 'https://isoflow.io',
+    tags: ['流程图']
+  },
+  {
+    id: 21,
+    name: 'Carbon',
+    desc: '把代码转成好看的图片',
+    url: 'https://carbon.now.sh',
+    tags: ['开发', '代码转图']
+  },
+  {
+    id: 22,
+    name: 'Codeimg',
+    desc: '把代码转成好看的图片',
+    url: 'https://codeimg.io',
+    tags: ['开发', '代码转图']
+  },
+  {
+    id: 23,
+    name: 'asciiflow',
+    desc: '使用 ASCII 绘制流程图，可以直接粘贴文本出来',
+    url: 'https://asciiflow.com',
+    tags: ['流程图']
+  },
+  {
+    id: 24,
+    name: 'Debuggex',
+    desc: '正则表达式在线调试',
+    url: 'https://www.debuggex.com',
+    tags: ['开发', '正则']
+  },
+  {
+    id: 25,
+    name: 'Happy Hues',
+    desc: '提供大量配色方案可以选择',
+    url: 'https://www.happyhues.co',
+    tags: ['配色']
+  },
+  {
+    id: 26,
+    name: 'Picular',
+    desc: '根据输入的名字提供配色方案，但是好像不是太准',
+    url: 'https://picular.co',
+    tags: ['配色']
+  },
+  {
+    id: 27,
+    name: 'Shields.io',
+    desc: '在线生成项目的标签，比如代码覆盖率、大小、协议、版本等',
+    url: 'https://shields.io',
+    tags: ['开发', '标签']
+  },
   {
     id: 28,
     name: 'UI BUTTONS',
@@ -68,7 +218,13 @@ const data: Data = [
     url: 'https://wttr.in',
     tags: ['天气', '命令行']
   },
-  { id: 32, name: 'Lorem Picsum', desc: '宠物、风景风格的图片占位符', url: 'https://picsum.photos', tags: ['开发', '图片', '占位符'] },
+  {
+    id: 32,
+    name: 'Lorem Picsum',
+    desc: '宠物、风景风格的图片占位符',
+    url: 'https://picsum.photos',
+    tags: ['开发', '图片', '占位符']
+  },
   {
     id: 33,
     name: 'Lorem Picsum',
@@ -83,7 +239,13 @@ const data: Data = [
     url: 'https://bundlephobia.com',
     tags: ['开发', 'NPM']
   },
-  { id: 35, name: 'Badgen', desc: '在线生成项目的标签，比如代码覆盖率、大小、协议、版本等', url: 'https://badgen.net', tags: ['开发', '标签'] },
+  {
+    id: 35,
+    name: 'Badgen',
+    desc: '在线生成项目的标签，比如代码覆盖率、大小、协议、版本等',
+    url: 'https://badgen.net',
+    tags: ['开发', '标签']
+  },
   {
     id: 36,
     name: 'SCREEN SIZ.ES',
@@ -91,7 +253,13 @@ const data: Data = [
     url: 'https://screensiz.es',
     tags: ['开发', '设备', '屏幕']
   },
-  { id: 37, name: 'CSS LAYOUT', desc: '使用 CSS 完成网页各种组件设计', url: 'https://csslayout.io', tags: ['开发', 'CSS', '布局'] },
+  {
+    id: 37,
+    name: 'CSS LAYOUT',
+    desc: '使用 CSS 完成网页各种组件设计',
+    url: 'https://csslayout.io',
+    tags: ['开发', 'CSS', '布局']
+  },
   {
     id: 38,
     name: 'CSS Gradient',
@@ -112,8 +280,49 @@ const data: Data = [
     desc: '数据库托管平台，有免费的版本',
     url: 'https://planetscale.com',
     tags: ['开发', '数据库']
+  },
+  {
+    id: 41,
+    name: 'CodeSanbox',
+    desc: '在线代码分享平台，有常用的框架模板，提供便捷的编辑体验，甚至可以开发演示小型项目',
+    url: 'https://codesandbox.io',
+    tags: ['开发', '在线代码']
+  },
+  {
+    id: 42,
+    name: 'CodePen',
+    desc: '在线代码分享平台，不过框架模版稀少，主要提供 HTML CSS JS 的演示和分享',
+    url: 'https://codepen.io',
+    tags: ['开发', '在线代码']
+  },
+  {
+    id: 43,
+    name: 'JSFiddle',
+    desc: '在线代码分享平台，没有模板，主要提供 HTML CSS JS 的演示和分享',
+    url: 'https://jsfiddle.net',
+    tags: ['开发', '在线代码']
+  },
+  {
+    id: 44,
+    name: '字节跳动静态资源公共库',
+    desc: '静态资源库，有大量的知名框架、库资源可以选择',
+    url: 'https://cdn.bytedance.com',
+    tags: ['开发', 'CDN', '静态资源']
+  },
+  {
+    id: 45,
+    name: 'Z-Library',
+    desc: '世界上最大的数字图书馆，有丰富的 PDF 图书等资源。入口网址可能需要科学上网，随后会显示出可访问的域名地址。未登录的用户每天支持下载 10 个资源',
+    url: 'https://zh.z-lib.org',
+    tags: ['图书']
+  },
+  {
+    id: 46,
+    name: 'Speedtest by Ookla',
+    desc: '最知名的全球测速网站',
+    url: 'https://www.speedtest.net',
+    tags: ['运维', '测速']
   }
-
 ]
 
 export default data
