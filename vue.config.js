7b503efb7007e322b18c8710b5b18fc0ecb83a80
yukapril <i@yukapril.com>
2022-08-14 12:37:46 +0800

    feat: pwa

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  productionSourceMap: false
+  productionSourceMap: false,
+  pwa: {
+    name: '网址收藏',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true
+    }
+  },
+  devServer: {
+    https: {
+      key: fs.readFileSync('/Users/lijingchen/Github/testCA/cert.key'),
+      cert: fs.readFileSync('/Users/lijingchen/Github/testCA/cert.crt'),
+      ca: fs.readFileSync('/Users/lijingchen/Github/testCA/ca.crt')
+    }
+  }
 })
